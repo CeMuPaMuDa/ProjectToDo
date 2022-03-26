99ede8d5d579fab86a5254015f11bbfddeaedaa7
CeMuPaMuDa <bepyc9i@gmail.com>
2022-03-27 00:36:08 +0200

    add locale configuration

--- config/application.rb
+++ config/application.rb
@@ -1,37 +1,23 @@
 require_relative "boot"
 
 require "rails"
-# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
-# require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
 require "action_view/railtie"
-# require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 require 'generators/slim/scaffold/scaffold_generator.rb'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ProjectToDo
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.available_locales = %i[en ru uk]
+    config.i18n.default_locale = :ru
+    config.time_zone = 'Kyiv'
     config.paths.add Rails.root.join('lib').to_s, eager_load: true
 
     initializer 'load_slim_ext' do
@@ -40,7 +26,6 @@ module ProjectToDo
       end
     end
 
-    # Don't generate system test files.
     config.generators do |g|
       g.orm              :active_record
       g.template_engine  :erb
