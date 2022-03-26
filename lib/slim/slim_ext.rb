deab90a9c36d4c8b88ffe071c3d239f46cdf3995
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-03-26 20:51:07 +0300

    Чистим от binding.pry

--- lib/slim/slim_ext.rb
+++ lib/slim/slim_ext.rb
@@ -2,7 +2,6 @@ module Slim::SlimExt
   def copy_view_files
     available_views.each do |view|
       filename = filename_with_extensions view
-      # binding.pry
       template(
         Rails.root.join('lib/templates', "#{view}.html.slim"),
         File.join('app', 'views', controller_file_path, filename)
