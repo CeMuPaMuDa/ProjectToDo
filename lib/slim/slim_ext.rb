c9040044a96ab14ccfacb9c2cc7ebd4059d36b37
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-03-26 20:32:34 +0300

    Отладка генерации новых шаблонов

--- lib/slim/slim_ext.rb
+++ lib/slim/slim_ext.rb
@@ -3,6 +3,7 @@ module Slim::SlimExt
     binding.pry
     available_views.each do |view|
       filename = filename_with_extensions view
+      binding.pry
       template "#{view}.html.slim", File.join('app', 'views', controller_file_path, filename)
     end
 
