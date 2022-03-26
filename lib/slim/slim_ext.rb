010294eb56531a1c77b26ce00a2f27c579dcea77
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-03-26 20:43:08 +0300

    Модифицируем copy_view_files

--- lib/slim/slim_ext.rb
+++ lib/slim/slim_ext.rb
@@ -3,8 +3,11 @@ module Slim::SlimExt
     binding.pry
     available_views.each do |view|
       filename = filename_with_extensions view
-      binding.pry
-      template "#{view}.html.slim", File.join('app', 'views', controller_file_path, filename)
+      # binding.pry
+      template(
+        Rails.root.join('lib/templates', "#{view}.html.slim"),
+        File.join('app', 'views', controller_file_path, filename)
+      )
     end
 
     template 'partial.html.slim', File.join('app/views', controller_file_path, "_#{singular_name}.html.slim")
