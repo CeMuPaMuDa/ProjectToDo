0d3473fa3e51f90469c990f29fcbed5f2fbef51b
CeMuPaMuDa <bepyc9i@gmail.com>
2022-03-26 21:37:26 +0200

    rubocop final

--- lib/slim/slim_ext.rb
+++ lib/slim/slim_ext.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
 
-module Slim::SlimExt
-  def copy_view_files
-    available_views.each do |view|
-      filename = filename_with_extensions view
-      template(
-        Rails.root.join('lib/templates', "#{view}.html.slim"),
-        File.join('app', 'views', controller_file_path, filename)
-      )
+module Slim
+  module SlimExt
+    def copy_view_files
+      available_views.each do |view|
+        filename = filename_with_extensions view
+        template(
+          Rails.root.join('lib/templates', "#{view}.html.slim"),
+          File.join('app', 'views', controller_file_path, filename)
+        )
+      end
+
+      template 'partial.html.slim', File.join('app/views', controller_file_path, "_#{singular_name}.html.slim")
     end
-
-    template 'partial.html.slim', File.join('app/views', controller_file_path, "_#{singular_name}.html.slim")
   end
 end
-
