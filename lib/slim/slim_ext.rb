661b07862a8d913bd1ca9469b8a2fb3b14fc54b0
CeMuPaMuDa <bepyc9i@gmail.com>
2022-03-26 21:32:29 +0200

    rubocop episod 2

--- lib/slim/slim_ext.rb
+++ lib/slim/slim_ext.rb
@@ -1,5 +1,4 @@
-# rubocop:disable Style/FrozenStringLiteralComment
-# rubocop:disable Style/ClassAndModuleChildren
+
 module Slim::SlimExt
   def copy_view_files
     available_views.each do |view|
@@ -13,5 +12,4 @@ module Slim::SlimExt
     template 'partial.html.slim', File.join('app/views', controller_file_path, "_#{singular_name}.html.slim")
   end
 end
-# rubocop:enable Style/FrozenStringLiteralComment
-# rubocop:enable Style/ClassAndModuleChildren
+
